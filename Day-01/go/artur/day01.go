c8a48c91c97527954be9f5fc4f011a702bcd7152
shizophrenicgopher <aureumapes@duck.com>
2025-12-12 09:06:25 +0100

    Day One with go

---
+++ Day-01/go/artur/day01.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("Day-01/go/artur/input.txt")
+	lines := strings.Split(string(input), "\n")
+
+	countP1 := 0
+	countP2 := 0
+	dial := int64(50)
+	for _, line := range lines {
+		increase, _ := strconv.ParseInt(line[1:], 10, 64)
+
+		for ; increase != 0; increase-- {
+			if line[0] == 'R' {
+				dial = (dial + 1) % 100
+			} else {
+				dial = (dial - 1) % 100
+			}
+			if dial == 0 {
+				countP2++
+			}
+		}
+		if dial == 0 {
+			countP1++
+		}
+	}
+	println(countP1)
+	println(countP2)
+}
